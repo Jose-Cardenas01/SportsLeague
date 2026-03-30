b39d1bb720417583f69741f2fe80db4e6f161658
Jose Cardenas <josecardenas0045@gmail.com>
2026-03-29 20:41:46 -0500

    .DataAccess structure

---
+++ SportsLeague.DataAccess/Context/LeagueDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SportsLeague.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SportsLeague.DataAccess.Context
+{
+    public class LeagueDbContext : DbContext
+    {
+        public LeagueDbContext(DbContextOptions<LeagueDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Teams> Teams { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Teams>(entity =>
+            {
+                entity.HasIndex(t => t.Name).IsUnique();
+            });
+        }
+    }
+}
